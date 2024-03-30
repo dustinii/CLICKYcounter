7718dd57216a2988e006a28245498a291ab2a8f4
dustinii <125335987+dustinii@users.noreply.github.com>
2024-03-30 16:26:31 -0700

    add increment and reset buttons

---
+++ Counter.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { useDispatch, useSelector } from 'react-redux';
+  import store from '../store';
+
+  export let count;
+</script>
+
+<div class="flex flex-col items-center justify-center h-screen">
+  <h1 class="text-3xl font-bold mb-8">Clicky Counter</h1>
+  <div class="text-5xl font-bold">{count}</div>
+  <div class="flex mt-8 space-x-4">
+    <button type="button" class="btn btn-primary" on:click={() => dispatch(store.dispatch(incrementCounter()))}>
+      Increment
+    </button>
+    <button type="button" class="btn btn-secondary" on:click={() => dispatch(store.dispatch(resetCounter()))}>
+      Reset
+    </button>
+  </div>
+</div>
+
+<style>
+
+</style>
