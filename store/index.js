678d455098dc532f919dfc398fa8811209521fd9
dustinii <125335987+dustinii@users.noreply.github.com>
2024-04-05 11:33:00 -0700

    add history

--- store/index.js
+++ store/index.js
@@ -4,12 +4,12 @@ const initialState = { count: 0 };
 
 const counterReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'INCREMENT':
-      return { ...state, count: state.count + 1 };
-    case 'RESET':
-      return { ...state, count: action.payload };
-    default:
-      return state;
+  case 'INCREMENT':
+    return { ...state, count: state.count + 1, history: [state.count, ...state.history.slice(0, 4)] }; // Limit history length to 4
+  case 'RESET':
+    return { ...state, count: action.payload, history: [action.payload] };
+  default:
+    return state;
   }
 };
 
