d6a95972def600290b90a5042041399ca25ef349
dustinii <125335987+dustinii@users.noreply.github.com>
2024-04-06 16:24:43 -0700

    add decrementCounter function

--- store/actions.js
+++ store/actions.js
@@ -2,6 +2,10 @@ export const incrementCounter = () => ({
   type: 'INCREMENT',
 });
 
+const decrementCounter = () => ({
+  type: 'DECREMENT',
+});
+
 export const resetCounter = () => ({
   type: 'RESET',
   payload: Math.floor(Math.random() * 10) + 1,
