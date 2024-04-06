fef38ffaa3f2298ac9cf0ad1b902260e623fa8e5
dustinii <125335987+dustinii@users.noreply.github.com>
2024-04-06 16:29:13 -0700

    add decrement case return

--- store/index.js
+++ store/index.js
@@ -6,6 +6,8 @@ const counterReducer = (state = initialState, action) => {
   switch (action.type) {
   case 'INCREMENT':
     return { ...state, count: state.count + 1, history: [state.count, ...state.history.slice(0, 4)] }; // Limit history length to 4
+  case 'DECEREMENT':
+    return {...state, count: Math.max(0, state.count - 1)};
   case 'RESET':
     return { ...state, count: action.payload, history: [action.payload] };
   default:
